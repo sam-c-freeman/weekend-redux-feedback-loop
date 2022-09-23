97e4e6578dab3a860ebd883f778695e0205ca06f
Sam Freeman <samcfreeman888@gmail.com>
2022-09-23 10:48:33 -0500

    need to install react-redux

---
+++ src/components/FeelingForm/FeelingForm.jsx
@@ -0,0 +1,27 @@
+import {useState} from 'react';
+import {useHistory} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+
+function FeelingForm (){
+    const[feelingRating, setFeelingRating] = useState('');
+    //what should default rating be?
+    //How will I clear it?
+    const handleNext = () =>{
+        console.log(feelingRating);
+    }
+
+    return(
+        <form onSubmit={handleNext}>
+            <div className="radioButtons"  onChange={(event) => setFeelingRating(event.target.value)}>
+                    <input type="radio" value="1" name="feeling"/> 1
+                    <input type="radio" value="2" name="feeling"/> 2
+                    <input type="radio" value="3" name="feeling"/> 3
+                    <input type="radio" value="4" name="feeling"/> 4
+                    <input type="radio" value="5" name="feeling"/> 5
+            </div>
+            <button>Next</button>
+        </form>
+    );
+}
+
+export default FeelingForm;
